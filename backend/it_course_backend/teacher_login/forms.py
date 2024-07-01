05d8d3a0072818a51b728a7ff70f3c5b0782ba5c
uni123234 <uni2.0zxc@gmail.com>
2024-07-01 12:42:55 +0200

    add

---
+++ backend/it_course_backend/teacher_login/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.username_field.verbose_name},
+                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return cleaned_data
+
+    def get_user(self):
+        return self.user_cache
