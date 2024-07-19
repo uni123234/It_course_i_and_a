331c1972d31f8050430a04fff1891512704cc3a5
uni123234 <uni2.0zxc@gmail.com>
2024-07-19 14:32:37 -0300

    add

--- backend/it_course_backend/login/views.py
+++ backend/it_course_backend/login/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login as auth_login
 from .forms import LoginForm
+from .models import LoginAttempt
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 @csrf_exempt
 def login(request):
@@ -11,8 +15,13 @@ def login(request):
         form = LoginForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
+            auth_login(request, user)
+            LoginAttempt.objects.create(user=user, timestamp=timezone.now())
             return JsonResponse({"message": "Login successful"}, status=200)
         else:
+            username = request.POST.get('username')
+            user = User.objects.filter(username=username).first()
+            if user:
+                LoginAttempt.objects.create(user=user, timestamp=timezone.now())
             return JsonResponse({"errors": form.errors}, status=400)
     return JsonResponse({"error": "Invalid request method"}, status=400)
--- backend/it_course_backend/register/views.py
+++ backend/it_course_backend/register/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .forms import RegistrationForm
+from .models import SignInAttempt
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 @csrf_exempt
 def register(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            SignInAttempt.objects.create(user=user, timestamp=timezone.now())
             return JsonResponse({"message": "Registration successful"}, status=201)
         else:
             return JsonResponse({"errors": form.errors}, status=400)
