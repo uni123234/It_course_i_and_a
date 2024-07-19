68f08bec20c3050d58e6c5608931321359861dad
Vano0928 <vanyapro57@gmail.com>
2024-07-19 20:06:41 +0300

    уккщк

--- backend/it_course_backend/register/forms.py
+++ backend/it_course_backend/register/forms.py
@@ -8,4 +8,4 @@ User = get_user_model()
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "password")
