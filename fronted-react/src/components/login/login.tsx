45e70fe29398ca3ecdac67a71322486110a8e5f7
Vano0928 <vanyapro57@gmail.com>
2024-08-26 22:45:40 +0300

    added login html/css

---
+++ fronted-react/src/components/login/login.tsx
@@ -0,0 +1,32 @@
+import './login.css'
+
+function Login() {
+
+  return (
+    <>
+        <div className="container">
+            <div className="image-section"></div>
+            <div className="form-section">
+                <div className="form-container">
+                    <h2>Welcome Back!</h2>
+                    <form>
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" id="email" name="email" placeholder="example@mail.com" />
+                        {/* <p *ngIf="loginForm.submitted && emailInvalid">Please write the correct email.</p> */}
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="********" />
+                        {/* <p *ngIf="loginForm.submitted && passwordInvalid">Password must be at least 6 characters long.</p> */}
+                        <button type="submit">Login</button>
+                        {/* <p *ngIf="credentialsError">{{ credentialsError }}</p> */}
+                        <div className="signup-link">
+                            Don't have an account yet? <a href="/register">Create an account</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Login
--- fronted-react/src/main.tsx
+++ fronted-react/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Login from './components/login/login.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
