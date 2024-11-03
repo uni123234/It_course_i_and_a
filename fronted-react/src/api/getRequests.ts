0cd66500e1dbc5e6e90a4c905ca225699fd4b342
Vano0928 <vanyapro57@gmail.com>
2024-11-03 11:52:05 +0200

    AAAAA

--- fronted-react/src/api/getRequests.ts
+++ fronted-react/src/api/getRequests.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 import API_URL from "./index";
 
-const fetchData = async (endpoint: string, params: Record<string, any> = {}, token: string | null) => {
+const fetchData = async (
+  endpoint: string,
+  params: Record<string, any> = {},
+  token: string | null
+) => {
   try {
     const response = await axios.get(`${API_URL}${endpoint}`, {
       headers: {
@@ -10,7 +14,7 @@ const fetchData = async (endpoint: string, params: Record<string, any> = {}, tok
       params,
     });
 
-    console.log(response.data)
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(`Error fetching data from ${endpoint}:`, error);
@@ -18,10 +22,15 @@ const fetchData = async (endpoint: string, params: Record<string, any> = {}, tok
   }
 };
 
-export const getCourses = (token: string | null) => fetchData("/course/", {}, token);
-export const getReminders = (token: string | null) => fetchData("/reminders/", {}, token);
-export const fetchCourse = (courseId: number, token: string | null) => fetchData(`/course/${courseId}/`, {}, token);
-export const getLessons = (token: string | null) => fetchData("/lessons/", {}, token);
-export const getCalendar = (token: string | null) => fetchData("/calendar/", {}, token);
+export const getCourses = (token: string | null) =>
+  fetchData("/course/", {}, token);
+export const getReminders = (token: string | null) =>
+  fetchData("/reminders/", {}, token);
+export const fetchCourse = (courseId: number, token: string | null) =>
+  fetchData(`/course/${courseId}/`, {}, token);
+export const getLessons = (courseId: number, token: string | null) =>
+  fetchData("/lessons/", { course_id: courseId }, token);
+export const getCalendar = (token: string | null) =>
+  fetchData("/calendar/", {}, token);
 export const getHomeworks = (courseId: number, token: string | null) =>
   fetchData("/homework/", { course_id: courseId }, token);
